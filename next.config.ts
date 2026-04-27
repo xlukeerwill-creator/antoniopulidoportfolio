5139037b3efe2a63193e3f2f5056992c1666f66a
Antonio José Pulido Sáez <apulido18@MacBook-Air-de-Antonio.local>
2026-04-27 12:09:35 -0400

    Añadir redireccion 301 the-lawyer-learning-to-code -> on-being-a-beginner-again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/thoughts/the-lawyer-learning-to-code",
+        destination: "/thoughts/on-being-a-beginner-again",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
